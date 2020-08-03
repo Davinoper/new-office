179e94b5b83fc2057232acc02cfcc0f8373a1563
Davinoper <63315718+Davinoper@users.noreply.github.com>
2020-08-03 16:31:43 -0300

    Update email.js

--- js/email.js
+++ js/email.js
@@ -11,5 +11,5 @@ function email2(){
     var nome = document.getElementById("nome").value;
     var telefone = document.getElementById("fone").value;
     console.log(mensagem)
-    window.location.href = "mailto:newoffice@gmail.com? Subject: cu &body= Nome: " + nome + ", "  + " telefone:" + telefone + ", " + "Sobre sa carga: "+ mensagem;
+    window.location.href = "mailto:newoffice@gmail.com? Subject: cu &body= Nome: " + nome + ", "  + " telefone:" + telefone + ", " + "Sobre a carga: "+ mensagem;
 } 
