bb8eb4d474e07acb62404ac35ed70cfae46167cc
Davinoper <63315718+Davinoper@users.noreply.github.com>
2020-08-02 21:56:55 -0300

    Add files via upload

--- js/email.js
+++ js/email.js
@@ -5,3 +5,11 @@ function email(){
     console.log(mensagem)
     window.location.href = "mailto:newoffice@gmail.com? Subject: cu &body= Nome: " + nome + ", "  + " telefone:" + telefone + ", " + mensagem;
 } 
+
+function email2(){
+    var mensagem = document.getElementById("comentarios").value;
+    var nome = document.getElementById("nome").value;
+    var telefone = document.getElementById("fone").value;
+    console.log(mensagem)
+    window.location.href = "mailto:newoffice@gmail.com? Subject: cu &body= Nome: " + nome + ", "  + " telefone:" + telefone + ", " + "Sobre sua carga: "+ mensagem;
+} 
